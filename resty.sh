3c7fe83003d20fb736f3f4389abe567ce72fb94c
TsT <tst2005@gmail.com>
2015-03-03 02:53:53 +0100

    all steps moved into functions

--- resty.sh
+++ resty.sh
@@ -46,64 +46,97 @@ case "$profiledir" in
 esac
 
 
-# check if already installed ?
-[ ! -d "$installdir" ] || stop "because $name seems already installed in $installdir"
+download_steps() {
 
-# download the source code archive
-[ -f "$target.tar.gz"     ] || wget 'http://openresty.org/download/'"$target"'.tar.gz'     || stop "Fail to download $target.tar.gz"
-[ -f "$target.tar.gz.asc" ] || wget 'http://openresty.org/download/'"$target"'.tar.gz.asc' || stop "Fail to download $target.tar.gz.asc"
+	# download the source code archive
+	[ -f "$target.tar.gz"     ] || wget 'http://openresty.org/download/'"$target"'.tar.gz'     || stop "Fail to download $target.tar.gz"
+	[ -f "$target.tar.gz.asc" ] || wget 'http://openresty.org/download/'"$target"'.tar.gz.asc' || stop "Fail to download $target.tar.gz.asc"
 
-# check and trust the source code archive
-LANG=C LANGUAGE=C gpg --verify "$target.tar.gz.asc" 2>&1 | grep -q -F 'gpg: Good signature from "Yichun Zhang (agentzh) <agentzh@gmail.com>' || stop "[SECURITY] fail to verify the archive ! TAKE CARE !"
+	# check and trust the source code archive
+	LANG=C LANGUAGE=C gpg --verify "$target.tar.gz.asc" 2>&1 | grep -q -F 'gpg: Good signature from "Yichun Zhang (agentzh) <agentzh@gmail.com>' || stop "[SECURITY] fail to verify the archive ! TAKE CARE !"
 
-# extract the archive
-[ -d "$target" ] || tar -xvzf "$target.tar.gz" || stop "at archive extraction"
+	# extract the archive
+	[ -d "$target" ] || tar -xvzf "$target.tar.gz" || stop "at archive extraction"
 
-# enter into the directory
-cd -- "$target" || stop "at change directory $target"
+	# enter into the directory
+	cd -- "$target" || stop "at change directory $target"
 
-# simple check before configure
-[ -f ./configure ] || stop "at pre-configure step : missing configure file"
+	# simple check before configure
+	[ -f ./configure ] || stop "at pre-configure step : missing configure file"
+}
+
+download_steps
+
 
 ################################
 ### debian dependencies part ###
 
-#dpkg -l libluajit-5.1-dev:amd64 >/dev/null 2>&1
-apt-get install -s libluajit-5.1-dev:amd64 >/dev/null || stop "debian package libluajit-5.1-dev:amd64 seems not available"
+deps_steps() {
+	#dpkg -l libluajit-5.1-dev:amd64 >/dev/null 2>&1
+	apt-get install -s libluajit-5.1-dev:amd64 >/dev/null || stop "debian package libluajit-5.1-dev:amd64 seems not available"
 
-apt-get install -s libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl make || stop "at simulation of debian packages"
+	apt-get install -s libreadline-dev libncurses5-dev libpcre3-dev libssl-dev perl make || stop "at simulation of debian packages"
+}
 
 ################################
 
-# must have ldconfig in path
-PATH="$PATH:/sbin"; export PATH
-type ldconfig || stop "at ldconfig not found (even with /sbin in PATH)"
 
-# clean
-[ ! -f "Makefile" ] || make clean
+compile_steps() {
 
-# configure
-{
-./configure \
---with-luajit \
---prefix="$installdir" \
-$(
-off() { echo "--without-${1#--without-}"; }
-while read w1 w2 w3_; do
-	if [ "$w1" = "#" ] || [ -z "$w1" ]; then continue; fi;
-	case "$w1" in
-		off) "$w1" "$w2" ;;
-	esac
-done < "$options"
-)
-} || stop "at configure step"
+	# must have ldconfig in path
+	PATH="$PATH:/sbin"; export PATH
+	type ldconfig || stop "at ldconfig not found (even with /sbin in PATH)"
 
-# make
-make || stop "at make step"
+	# clean
+	[ ! -f "Makefile" ] || make clean
 
+	# configure
+	{
+	./configure \
+	--with-luajit \
+	--prefix="$installdir" \
+	$(
+		off() { echo "--without-${1#--without-}"; }
+		while read w1 w2 w3_; do
+			if [ "$w1" = "#" ] || [ -z "$w1" ]; then continue; fi;
+			case "$w1" in
+				off) "$w1" "$w2" ;;
+			esac
+		done < "$options"
+	)
+	} || stop "at configure step"
 
-# install
-make install || stop "at install step"
+	# make
+	make || stop "at make step"
+}
 
-echo "# Installed in $installdir"
-echo "# Status: OK"
+install_steps() {
+
+	umask 022 ;# FIXME: find a better way to fix permission
+
+	# install
+	make install || stop "at install step"
+
+	echo "# Installed in $installdir"
+}
+
+post_install_check() {
+	[ -f "$installdir/bin/resty" ] || stop "at post_install_check: resty binary not found (install fail?)"
+	[ -x "$installdir/bin/resty" ] || stop "at post_install_check: resty binary is not executable (really ?!)"
+
+	# show the version ... TODO: do some check with this string.
+	"$installdir/bin/resty" -V 2>&1 | grep ^nginx
+}
+
+# check if already installed ?
+if [ ! -d "$installdir" ]; then
+	download_steps
+	deps_steps
+	compile_steps
+	install_steps
+#else
+	#stop "because $name seems already installed in $installdir"
+fi
+post_install_check
+
+echo "OK installed in $installdir"
